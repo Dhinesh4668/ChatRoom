310170d53a08c2e48306f07d4d7f44533cf0d95e
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-02-27 21:50:50 +0530

    configruation

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./App.{js,jsx,ts,tsx}",
+  "./screens/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    "nativewind/babel"
+  ],
+}

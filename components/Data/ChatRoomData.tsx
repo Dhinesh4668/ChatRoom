779d0ffc86eeddca6a02d237946baec4450828a2
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-03-01 00:03:52 +0530

    navigation properti 😍🦊🦊Ⓜ️😃😃😃

--- components/Data/ChatRoomData.tsx
+++ components/Data/ChatRoomData.tsx
@@ -1,56 +1,146 @@
-export default [
-  {
-    id: '1',
-    users: [
-      {
-        id: 'u1',
-        name: 'Dhinesh Kumar',
-        imgUrl:
-          'https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true',
-      },
-      {
-        id: 'u2',
-        name: 'Preethika',
-        imgUrl:
-          'https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true',
-      },
-      {
-        id: 'u3',
-        name: 'Moorthi',
-        imgUrl:
-          'https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true',
-      }
-      ],
-      latestMassage: {
-        id: 'm1',
-        content: 'hello, projectexpo cordinaters this is dhinesh kumar application and web devloper in the react and react native js lib',
-        createdAt: '2023-02-28:11:37PM'
-      },
+export default [{
+  id: '1',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u2',
+    name: 'Elon Musk',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+  }],
+  lastMessage: {
+    id: 'm1',
+    content: 'btw, SpaceX is interested in buying notJust.dev!',
+    createdAt: '2020-10-03T14:48:00.000Z',
   },
-  {
-    id: '2',
-    users: [
-      {
-        id: 'u1',
-        name: 'user1',
-        imgUrl:
-          'https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true',
-      },
-      {
-        id: 'u2',
-        name: 'user2',
-        imgUrl:
-          'https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true',
-      },
-      {
-        id: 'u3',
-        name: 'user3',
-        imgUrl:
-          'https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true',
-      }],
-      latestMassage: {
-        id: 'm1',
-        content: 'hello, projectexpo cordinaters this is dhinesh kumar application and web devloper in the react and react native js lib',
-        createdAt: '2023-02-28:11:37PM'
-      },
-  }];
+  newMessages: 4,
+}, {
+  id: '231231231231231',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u3',
+    name: 'Jeff',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+  }],
+  lastMessage: {
+    id: 'm2',
+    content: 'Why did you reject our offer?',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '3',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u4',
+    name: 'Zuck',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
+  }],
+  lastMessage: {
+    id: 'm3',
+    content: 'Is signal really better than my Whatsapp?',
+    createdAt: '2020-10-02T14:48:00.000Z',
+  }
+}, {
+  id: '4',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u5',
+    name: 'Graham',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/graham.jpg',
+  }],
+  lastMessage: {
+    id: 'm4',
+    content: 'Destroy the like button!',
+    createdAt: '2020-09-29T14:48:00.000Z',
+  }
+}, {
+  id: '5',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u6',
+    name: 'Biahaze',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/biahaze.jpg',
+  }],
+  lastMessage: {
+    id: 'm5',
+    content: 'I would be happy',
+    createdAt: '2020-09-30T14:48:00.000Z',
+  }
+}, {
+  id: '6',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u7',
+    name: 'Sus?',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+  }],
+  lastMessage: {
+    id: 'm6',
+    content: 'Who sus?',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '7',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u8',
+    name: 'Daniel',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/2.jpg',
+  }],
+  lastMessage: {
+    id: 'm7',
+    content: 'How are you doing?',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '8',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u9',
+    name: 'Carlos',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/3.jpg',
+  }],
+  lastMessage: {
+    id: 'm8',
+    content: 'Hola hola coca cola?',
+    createdAt: '2020-09-27T15:40:00.000Z',
+  }
+}, {
+  id: '9',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+  }, {
+    id: 'u10',
+    name: 'Angelina Jolie',
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg',
+  }],
+  lastMessage: {
+    id: 'm9',
+    content: 'Meet me at the same place',
+    createdAt: '2020-09-25T15:40:00.000Z',
+  },
+}]

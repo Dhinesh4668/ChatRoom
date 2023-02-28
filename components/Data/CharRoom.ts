6dbc83512cb8b23b62f5367ed2017c521d4bae1c
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-02-28 21:35:55 +0530

    navigation

---
+++ components/Data/CharRoom.ts
@@ -0,0 +1,19 @@
+export default [
+  {
+    id: '1',
+    user: [
+      {
+        id: 'u1',
+        name: 'Dhinesh',
+        imageUri:
+          'https://www.tenforums.com/geek/gars/images/2/types/thumb_Picture.png',
+      },
+      {
+        id: 'u2',
+        name: 'Kumar',
+        imageUri:
+          'https://www.tenforums.com/geek/gars/images/2/types/thumb_Picture.png',
+      },
+    ],
+  },
+];
--- src/Screens/Chat/Chat.tsx
+++ src/Screens/Chat/Chat.tsx
@@ -1,5 +1,5 @@
 import react from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput,} from 'react-native';
 const Chat = () => {
   return (
     <>
